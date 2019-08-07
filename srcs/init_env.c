a3b45f3c87035cf31f0675aed3b4ce409233ba04
Etienne FISCHER <efischer@e1r2p17.42.fr>
2019-08-07 11:33:29 +0200

    init_env: reset SHLVL to 1 when SHLVL > 1 OK

--- srcs/init_env.c
+++ srcs/init_env.c
@@ -6,17 +6,36 @@
 /*   By: efischer <efischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/05 13:59:03 by efischer          #+#    #+#             */
-/*   Updated: 2019/08/05 13:59:27 by efischer         ###   ########.fr       */
+/*   Updated: 2019/08/07 11:31:43 by efischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static void	increase_shlvl(char *shlvl, t_list **lst)
+{
+	int		lvl_val;
+	char	*env_var[3];
+
+	lvl_val = ft_atoi(shlvl);
+	if (lvl_val + 1 < 0)
+		shlvl = ft_strdup("0");
+	else if (lvl_val + 1 >= 1000)
+	{
+		ft_dprintf(2, "minishell: warning: shell level (%ld) too high, resetting to 1\n", lvl_val + 1);
+		shlvl = ft_strdup("1");
+	}
+	else
+		shlvl = ft_itoa(lvl_val + 1);
+	put_name_val_in_tab("SHLVL", shlvl, env_var);
+	setenv_blt(env_var, lst);
+	ft_strdel(&shlvl);
+}
+
 static void	set_shlvl(t_list **lst)
 {
 	char	*shlvl;
 	char	*env_var[3];
-	int		lvl_val;
 
 	shlvl = ft_getenv("SHLVL", *lst);
 	if (shlvl == NULL)
@@ -25,16 +44,7 @@ static void	set_shlvl(t_list **lst)
 		setenv_blt(env_var, lst);
 	}
 	else
-	{
-		lvl_val = ft_atoi(shlvl);
-		if (lvl_val + 1 > 0)
-			shlvl = ft_itoa(lvl_val + 1);
-		else
-			shlvl = ft_strdup("0");
-		put_name_val_in_tab("SHLVL", shlvl, env_var);
-		setenv_blt(env_var, lst);
-		ft_strdel(&shlvl);
-	}
+		increase_shlvl(shlvl, lst);
 }
 
 void		init_env(t_list **lst)
